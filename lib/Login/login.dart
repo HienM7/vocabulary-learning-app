aed488b005ac92d819fb4dc9f48ae6edc6793ed7
nhat17t3 <58282021+nhat17t3@users.noreply.github.com>
2021-03-22 19:24:26 +0700

    Login and register (nhat and luan)

---
+++ lib/Login/login.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:vocabulary_learning_app/Home/home.dart';
+import 'package:vocabulary_learning_app/Register/register.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _showPass = false;
+  TextEditingController _userController = new TextEditingController();
+  TextEditingController _passController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+        constraints: BoxConstraints.expand(),
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+              child: Container(
+                  width: 70,
+                  height: 70,
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Color(0xffd8d8d8)),
+                  child: FlutterLogo()),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 60),
+              child: Text(
+                "LOGIN",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                    fontSize: 30),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+              child: TextField(
+                controller: _userController,
+                style: TextStyle(fontSize: 18, color: Colors.black),
+                decoration: InputDecoration(
+                    labelText: "USERNAME",
+                    labelStyle:
+                        TextStyle(color: Color(0xff888888), fontSize: 15)),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+              child: Stack(
+                alignment: AlignmentDirectional.centerEnd,
+                children: <Widget>[
+                  TextField(
+                    controller: _passController,
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                    obscureText: !_showPass,
+                    decoration: InputDecoration(
+                        labelText: "PASSWORD",
+                        labelStyle:
+                            TextStyle(color: Color(0xff888888), fontSize: 15)),
+                  ),
+                  GestureDetector(
+                    onTap: onToggleShowPass,
+                    child: Text(
+                      _showPass ? "HIDE" : "SHOW",
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontSize: 13,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 56,
+                child: RaisedButton(
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  onPressed: onSignInClicked,
+                  child: Text(
+                    "SIGN IN",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 130,
+              width: double.infinity,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () => {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => RegisterPage()))
+                    },
+                    child: Text(
+                      "NEW USER ? SIGN UP",
+                      style: TextStyle(fontSize: 15, color: Colors.blue),
+                    ),
+                  ),
+                  Text(
+                    "FORGOT PASSWORD ?",
+                    style: TextStyle(fontSize: 15, color: Colors.blue),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void onToggleShowPass() {
+    setState(() {
+      _showPass = !_showPass;
+    });
+  }
+
+  void onSignInClicked() {
+    setState(() {
+      Navigator.push(context, MaterialPageRoute(builder: gotoHome));
+    });
+  }
+
+  Widget gotoHome(BuildContext context) {
+    return HomePage();
+  }
+}

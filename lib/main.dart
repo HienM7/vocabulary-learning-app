cfc7b1c83899512640b7e3b7b2fd7a436dcb328c
nhat17t3 <58282021+nhat17t3@users.noreply.github.com>
2021-03-22 19:24:26 +0700

    Login and register (nhat and luan)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:vocabulary_learning_app/Screens/login/login.dart';
+import 'Login/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,19 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      // home: MyHomePage(title: 'Flutter Demo Home Page'),
-      home: LoginScreen(),
+      home: LoginPage(),
     );
   }
 }

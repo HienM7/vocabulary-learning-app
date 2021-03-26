69d70d610d972fd4300dddb884fc09aecd23eaba
Maivanhien <57941283+Maivanhien@users.noreply.github.com>
2021-03-26 15:45:00 +0700

    create home page and connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,11 @@ import 'package:vocabulary_learning_app/Screens/vocab_list/vocab_list.dart';
 import 'package:vocabulary_learning_app/Home/home.dart';
 import 'package:vocabulary_learning_app/Home/home_page.dart';
 import 'package:vocabulary_learning_app/Screens/course/course.dart';
+import 'package:vocabulary_learning_app/Home/home.dart';
+import 'package:vocabulary_learning_app/Home/home_page.dart';
+import 'Home/home.dart';
 
-void main() {
-  // SystemChrome.setEnabledSystemUIOverlays([]);
+void main(){
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }

13b5d3c1e99ebfe1847d976cd0053d961b00a1b0
Maivanhien <57941283+Maivanhien@users.noreply.github.com>
2021-03-26 15:45:00 +0700

    create home page and connect firebase

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,15 @@ import 'package:vocabulary_learning_app/constants/constants.dart';
 import 'package:vocabulary_learning_app/Login/login.dart';
 // import 'package:vocabulary_learning_app/Screens/login/login.dart';
 import 'package:vocabulary_learning_app/Register/register.dart';
+import 'package:vocabulary_learning_app/Home/home.dart';
+import 'package:vocabulary_learning_app/Home/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+import 'Home/home.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -29,6 +36,8 @@ class MyApp extends StatelessWidget {
               '/login': (context) => LoginPage(),
               '/signup': (context) => RegisterPage(),
               '/my-profile': (context) => ProfileScreen(),
+              '/home': (context) => HomePage(),
+              '/home-page': (context) => HomePageUser(),
             },
 
             home: LoginPage(),

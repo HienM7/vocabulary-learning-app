9443deca9fb6406728fa8e6b6a58f43159d453eb
Maivanhien <57941283+Maivanhien@users.noreply.github.com>
2021-04-08 07:36:31 +0700

    ListWord

--- lib/Home/home.dart
+++ lib/Home/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:vocabulary_learning_app/Home/widgets/info_text.dart';
-import 'package:vocabulary_learning_app/Home/widgets/bottom_bar_colum.dart';
+import 'package:vocabulary_learning_app/Screens/Shared/footer.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -349,69 +348,8 @@ class _HomePageState extends State<HomePage> {
                 )
               ],
             ),
-            Container(
-              padding: EdgeInsets.all(30),
-              margin: EdgeInsets.fromLTRB(0, 30, 0, 0),
-              color: Colors.blueGrey[900],
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      BottomBarColumn(
-                        heading: 'ABOUT',
-                        s1: 'Contact Us',
-                        s2: 'About Us',
-                        s3: 'Careers',
-                      ),
-                      BottomBarColumn(
-                        heading: 'HELP',
-                        s1: 'Payment',
-                        s2: 'Cancellation',
-                        s3: 'FAQ',
-                      ),
-                      BottomBarColumn(
-                        heading: 'SOCIAL',
-                        s1: 'Twitter',
-                        s2: 'Facebook',
-                        s3: 'YouTube',
-                      ),
-                      Container(
-                        color: Colors.blueGrey,
-                        width: 2,
-                        height: 150,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          InfoText(
-                            type: 'Email',
-                            text: 'explore@gmail.com',
-                          ),
-                          SizedBox(height: 5),
-                          InfoText(
-                            type: 'Address',
-                            text: '128, Trymore Road, Delft, MN - 56124',
-                          )
-                        ],
-                      ),
-                    ],
-                  ),
-                  Divider(
-                    color: Colors.blueGrey,
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Copyright © 2020 | EXPLORE',
-                    style: TextStyle(
-                      color: Colors.blueGrey[300],
-                      fontSize: 14,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            // Home footer
+            Footer(),
           ],
         )
       ),
